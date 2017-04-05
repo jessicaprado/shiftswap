513b1dbfb31f3aea6c701d3903a7f8f4d17d652b
Jessica Prado <jessica.marie.prado@gmail.com>
2017-04-05 16:15:59 -0400

    deployment

--- app/shiftSwap/config/oauth.js
+++ app/shiftSwap/config/oauth.js
@@ -2,8 +2,8 @@ module.exports = {
     facebookAuth: {
         clientID: '200500520435966',
         clientSecret: '4792884f49eb089ede3e5be7aa9bdb56',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
-        //callbackURL: 'https://shiftswapdemo.herokuapp.com/',
+        //callbackURL: 'http://localhost:3000/auth/facebook/callback'
+        callbackURL: 'https://shiftswapdemo.herokuapp.com/',
         //enableProof: false
     }
 }
--- app/shiftSwap/services/connection.js
+++ app/shiftSwap/services/connection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/shiftSwap');
-//mongoose.connect('mongodb://heroku_ldz76z2k:enjeidb40ang165tdv3ivlfcp0@ds151070.mlab.com:51070/heroku_ldz76z2k');
+//mongoose.connect('mongodb://localhost/shiftSwap');
+mongoose.connect('mongodb://heroku_ldz76z2k:enjeidb40ang165tdv3ivlfcp0@ds151070.mlab.com:51070/heroku_ldz76z2k');
 var db = mongoose.connection;
 
 db.on('error', function(error){
